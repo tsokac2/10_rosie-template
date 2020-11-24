48887aed2717145bf897ad7158d302969dbf44dd
tsokac2 <tsokac2@gmail.com>
2020-11-24 06:40:00 +0000

    Implement emailjs integration on contact page

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,18 @@
-console.log("Working");
+function sendMail(contactForm) {
+    emailjs.send("service_00cpd21", "rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function (response) {
+            console.log("SUCCESS", response);
+        },
+        function (error) {
+            console.log("FAILED", error);
+        });
+        return false;  // To block from loading a new page
+}
+
+
+
